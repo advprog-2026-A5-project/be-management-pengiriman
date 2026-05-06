084912ad607adeec57e73ea331377d4328c823dc
Kallln <ahmad.haikal41@ui.ac.id>
2026-05-06 14:10:19 +0700

    Init commit

---
+++ src/main/java/id/ac/ui/cs/advprog/bemanagementpengiriman/BeManagementPengirimanApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bemanagementpengiriman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeManagementPengirimanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeManagementPengirimanApplication.class, args);
+    }
+
+}
