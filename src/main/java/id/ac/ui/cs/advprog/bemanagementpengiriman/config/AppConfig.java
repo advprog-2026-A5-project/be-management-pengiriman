43b8790046af2c6e37e1e50cc3c3757567532c43
Kallln <ahmad.haikal41@ui.ac.id>
2026-05-16 19:53:11 +0700

    Add Config and Global exceptionHandler

---
+++ src/main/java/id/ac/ui/cs/advprog/bemanagementpengiriman/config/AppConfig.java
@@ -0,0 +1,9 @@
+package id.ac.ui.cs.advprog.bemanagementpengiriman.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@Configuration
+@EnableRetry
+public class AppConfig {
+}
---
+++ src/main/java/id/ac/ui/cs/advprog/bemanagementpengiriman/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package id.ac.ui.cs.advprog.bemanagementpengiriman.config;
+
+import id.ac.ui.cs.advprog.bemanagementpengiriman.security.HeaderAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(new HeaderAuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class)
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
