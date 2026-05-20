79fde0350fa072dfc9191fb0221d2106fb30d35d
Kallln <ahmad.haikal41@ui.ac.id>
2026-05-20 19:17:10 +0700

    [REFACTOR] Add validation for AdminPartialRejectionRequest

--- src/main/java/id/ac/ui/cs/advprog/bemanagementpengiriman/dto/AdminPartialRejectionRequest.java
+++ src/main/java/id/ac/ui/cs/advprog/bemanagementpengiriman/dto/AdminPartialRejectionRequest.java
@@ -5,11 +5,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class AdminPartialRejectionRequest {
+    @NotNull
+    @Positive
     private Double acknowledgedWeightKg;
+
+    @NotBlank
     private String rejectionReason;
 }
